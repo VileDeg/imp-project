67f3b3a4c91492c14fcf503fd78805fa6ae5b411
Vadim Goncearenco <gvadim134@gmail.com>
2023-11-03 16:36:50 +0100

    Minor code refactor/optimization

--- test_workspace.kds/demo/Sources/main.c
+++ test_workspace.kds/demo/Sources/main.c
@@ -1,12 +1,13 @@
 /* Header file with all the essential definitions for a given type of MCU */
 #include "MK60D10.h"
+#include "core_cm4.h"
 
 /* Constants specifying delay loop duration */
 #define tdelay1 32768
 #define tdelay2 	10
 
 /* Auxiliary global variable */
-int counter = 0;
+//int counter = 0;
 
 /* Variable delay loop */
 void delay(int t1, int t2)
@@ -50,13 +51,14 @@ void delay(int t1, int t2)
 #define D11 3
 #define D12 2
 
-const unsigned int ROW_ARR[8] = { R0, R1, R2, R3, R4, R5, R6, R7 };
 
 #define COL_MASK 0x00000d40
 #define ROW_MASK 0x3f000280
 #define EN_MASK  0x10000000
 
 #define LED_MASK 0x0000003C
+
+const unsigned int ROW_ARR[8] = { R0, R1, R2, R3, R4, R5, R6, R7 };
 
 // 16 * 8 = 128
 unsigned int field[128] = {
@@ -71,17 +73,6 @@ unsigned int field[128] = {
 		0,0,0,0, 0,0,0,0,  0,0,0,0, 0,0,0,0
 };
 
-//unsigned int field[128] = {
-//		0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0,
-//		0,0,0,0, 1,1,1,0, 1,1,1,0, 0,0,0,0,
-//		0,0,0,0, 1,0,1,0, 1,0,1,0, 0,0,0,0,
-//		0,0,0,0, 1,0,1,0, 1,1,1,0, 0,0,0,0,
-//
-//		0,0,0,0, 0,0,0,0, 0,0,0,0, 1,0,0,0,
-//		0,0,1,0, 0,1,0,1, 0,1,0,1, 0,1,0,0,
-//		0,0,0,1, 1,0,0,0, 1,0,0,1, 1,1,0,0,
-//		0,0,1,0, 0,1,0,1, 0,0,0,1, 0,1,0,0,
-//};
 
 enum direction {
 	STOP = -1, UP = 0, LEFT, DOWN, RIGHT
@@ -92,7 +83,9 @@ typedef struct {
 	unsigned int y;
 } pos_t;
 
-#define SNAKE_LEN 5
+#define SNAKE_LEN 6
+#define SNAKE_DISPLAY_LOOP_DELAY 1024
+#define SNAKE_SPEED 8
 
 int snake_dir = STOP;
 
@@ -101,8 +94,80 @@ pos_t snake_body[SNAKE_LEN] = {
 		{ 3, 10 },
 		{ 3, 11 },
 		{ 3, 12 },
-		{ 3, 13 }, // Tail
+		{ 3, 13 },
+		{ 3, 14 },
+		{ 3, 15 } // Tail
 };
+
+
+
+/*
+ *	This method configures the Multipurpose Clock Generator (MCG) to run in
+ *	Bypassed Low Power External (BLPE) mode from FEI mode. On the TWR-K60N512
+ *	Bypassed board, this would be 50 MHz.
+ *
+ */
+void MCG_FEI_BLPE() {
+	// Select External Reference Clock in MCG (Transition to FBE from FEI)
+	// C1[CLKS] written to 10
+	MCG->C1 = MCG_C1_CLKS(2);
+
+	// Wait for Reference clock to switch to external reference
+	while (MCG->S & MCG_S_IREFST_MASK);
+
+	// Wait for MCGOUT to switch over to the external reference clock
+	while (((MCG->S & MCG_S_CLKST_MASK) >> MCG_S_CLKST_SHIFT) != 0x2);
+
+	// Transition to bypassed low power external mode (BLPE)
+	// C2[LP] written to 1
+	MCG->C2 = MCG_C2_LP_MASK;
+}
+
+/*
+ *	This method configures the Multipurpose Clock Generator (MCG) to run in
+ *	FLL Engaged Internal (FEI) mode from BLPE mode. For K60 0M33Z masks, this
+ *	is 25 MHz, otherwise it is 20.97 MHz.
+ *
+ */
+void MCG_BLPE_FEI() {
+	// Transition to FBE
+	MCG->C2 = 0;
+
+	// Transition to FEI
+	MCG->C1 = MCG_C1_IREFS_MASK;
+
+	// Wait for Reference clock to switch to internal reference
+	while ((MCG->S & MCG_S_IREFST_MASK) == 0);
+
+	// Wait for MCGOUT to switch over to the FLL clock
+	while (((MCG->S & MCG_S_CLKST_MASK) >> MCG_S_CLKST_SHIFT) != 0x0);
+}
+
+
+/*
+ * Port E ISR
+ */
+void PORTE_IRQHandler() {
+	interrupt_button_handler();
+	// Clear all Port E interrupt flags
+	PORTE->ISFR = PORT_ISFR_ISF_MASK;
+}
+
+void PIT0_IRQHandler() {
+	// Clear the Interrupt Flag in the PIT module
+	PIT->CHANNEL[0].TFLG = 0x01;
+
+	// Toggle the LED
+	PTB->PTOR = (1 << 5);
+
+	if (snake_dir != STOP) {
+		update_snake_pos();
+	}
+}
+
+
+
+
 
 
 void init_led(void)
@@ -127,92 +192,8 @@ void init_buttons(void)
 	PORTE->PCR[SW6] = PORT_PCR_MUX(0x01); // SW6
 }
 
-void play_display_row_sequence(void)
-{
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R0 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R1 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R2 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R3 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R4 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R5 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R6 );
-	//delay(tdelay1, tdelay2);
-	PTE->PDOR |= GPIO_PDOR_PDO( 1 << R7 );
-}
-
-void play_display_col_sequence(void)
-{
-	unsigned int COL_ARR[4] = { A0, A1, A2, A3 };
-	unsigned int col_mask = (1 << A0) | (1 << A1) | (1 << A2) | (1 << A3);
-
-	//for (unsigned int i = 0; i < 16; ++i) {
-		for (unsigned int j = 0; j < 4; ++j) {
-			//unsigned int bit_j = i & (1 << j);
-			play_display_row_sequence();
-			// Disable all others
-			PTA->PDOR &= ~(GPIO_PDOR_PDO(col_mask));
-			// Enable the current one
-			PTA->PDOR |= GPIO_PDOR_PDO(1 << COL_ARR[j]);
-
-			delay(tdelay1, tdelay2);
-		}
-	//}
-}
-
-void buttons_handler(void)
-{
-	/* Evaluation of SW2 state */
-	if ( (PTE->PDIR & (1 << SW2)) != 0 ) {
-		PTB->PDOR |= (1 << D9);
-	} else {
-		snake_dir = UP;
-		PTB->PDOR &= ~(1 << D9);
-	}
-
-	/* Evaluation of SW3 state */
-	if ( (PTE->PDIR & (1 << SW3)) != 0 ) {
-		PTB->PDOR |= (1 << D10);
-	} else {
-		snake_dir = RIGHT;
-		PTB->PDOR &= ~(1 << D10);
-	}
-
-	/* Evaluation of SW4 state */
-	if ( (PTE->PDIR & (1 << SW4)) != 0 ) {
-		PTB->PDOR |= (1 << D11);
-	} else {
-		snake_dir = DOWN;
-		PTB->PDOR &= ~(1 << D11);
-	}
-
-	/* Evaluation of SW5 state */
-	if ( (PTE->PDIR & (1 << SW5)) != 0 ) {
-		PTB->PDOR |= (1 << D12);
-	} else {
-		snake_dir = LEFT;
-		PTB->PDOR &= ~(1 << D12);
-	}
-
-	/* Evaluation of SW6 state */
-	if ( (PTE->PDIR & (1 << SW6)) != 0 ) {
-		PTB->PDOR |= GPIO_PDOR_PDO( LED_MASK );
-	} else {
-		PTB->PDOR &= ~GPIO_PDOR_PDO( LED_MASK );
-	}
-}
-
 void init_display(void)
 {
-	//unsigned int col_mask = (1 << A0) | (1 << A1) | (1 << A2) | (1 << A3);
-	//unsigned int row_mask = (1 << R0) | (1 << R1) | (1 << R2) | (1 << R3) | (1 << R4) | (1 << R5) | (1 << R6) | (1 << R7);
-	//unsigned int en_mask  = 1 << EN; // 0001 0000 0x000000
-
 	// Set display EN to output
 	PORTE->PCR[EN] = ( PORT_PCR_MUX(0x01) );
 	// Set EN to 0 (MEANS ON) !!!
@@ -257,6 +238,50 @@ void init_display(void)
 	//PTA->PDOR &= ~( GPIO_PDOR_PDO( row_mask ) );
 }
 
+void init_pit(void)
+{
+	// Configure the Signal Multiplexer for SW6, configure SW6 to interrupt on falling edge
+	PORTE->PCR[SW2] = PORT_PCR_MUX(0x01) | PORT_PCR_IRQC(0b1010);
+	PORTE->PCR[SW3] = PORT_PCR_MUX(0x01) | PORT_PCR_IRQC(0b1010);
+	PORTE->PCR[SW4] = PORT_PCR_MUX(0x01) | PORT_PCR_IRQC(0b1010);
+	PORTE->PCR[SW5] = PORT_PCR_MUX(0x01) | PORT_PCR_IRQC(0b1010);
+
+	// Clear the Interrupt flags in Port E
+	PORTE->ISFR = 0xFFFFFFFF;
+
+	// Clear the Interrupt pending flag in the NVIC just in case
+	NVIC->ICPR[2] = (1 << 27);
+
+	// Enable the Interrupt in the NVIC
+	NVIC->ISER[2] = (1 << 27);
+
+	// Enable the clock on the PIT
+	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
+
+	// Turn on the PIT
+	PIT->MCR = 0x00;
+
+	// Load Timer Value into Channel 0
+	// LDVAL trigger = (period/clock period) - 1
+	// LDVAL trigger = (0.5 / (1 / 50,000,000)) - 1
+	PIT->CHANNEL[0].LDVAL = 24999999 / SNAKE_SPEED;
+
+	// Enable Interrupt and Start Timer
+	PIT->CHANNEL[0].TCTRL = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
+
+	//PIT->CHANNEL[1].LDVAL = 24999999 / 10000;
+
+	// Enable Interrupt and Start Timer
+	//PIT->CHANNEL[1].TCTRL = PIT_TCTRL_TIE_MASK | PIT_TCTRL_TEN_MASK;
+
+	// Enable PIT0 Interrupt Channel in NVIC
+	NVIC->ISER[2] |= (1 << 4);
+	// Enable PIT1 Interrupt Channel in NVIC
+	//NVIC->ISER[2] |= (1 << 5);
+}
+
+
+
 void play_led_sequence(void)
 {
 	//Turn all the LEDs sequentialy ON!
@@ -280,54 +305,65 @@ void play_led_sequence(void)
    	delay(tdelay1, tdelay2);
 }
 
-void set_col_on(unsigned int col_id)
-{
-	// Leave ROWs untouched
-	unsigned int pdor = PTA->PDOR & GPIO_PDOR_PDO( ROW_MASK );
-	// Add all column bits
-	pdor |= ((col_id >> 0) & 1u) << A0
-		  | ((col_id >> 1) & 1u) << A1
-		  | ((col_id >> 2) & 1u) << A2
-		  | ((col_id >> 3) & 1u) << A3;
 
-	// Set the final output
-	PTA->PDOR = pdor;
+
+
+void interrupt_button_handler(void)
+{
+	if ( (PORTE->ISFR & (1 << SW2)) != 0 ) {
+		PTB->PCOR = 1 << D9;
+		snake_dir = UP;
+	} else {
+		PTB->PSOR = 1 << D9;
+	}
+
+	if ( (PORTE->ISFR & (1 << SW3)) != 0 ) {
+		PTB->PCOR = 1 << D10;
+		snake_dir = RIGHT;
+	} else {
+		PTB->PSOR = 1 << D10;
+	}
+
+	if ( (PORTE->ISFR & (1 << SW4)) != 0 ) {
+		PTB->PCOR = 1 << D11;
+		snake_dir = DOWN;
+	} else {
+		PTB->PSOR = 1 << D11;
+	}
+
+	if ( (PORTE->ISFR & (1 << SW5)) != 0 ) {
+		PTB->PCOR = 1 << D12;
+		snake_dir = LEFT;
+	} else {
+		PTB->PSOR = 1 << D12;
+	}
 }
 
+
+void set_col_on(unsigned int col_id)
+{
+	// Set all cols OFF
+	PTA->PCOR = COL_MASK;
+	// Extract the individual bits from col_id and shift to the cols positions to create a mask
+	PTA->PSOR = ((col_id	 ) & 1u) << A0
+			  | ((col_id >> 1) & 1u) << A1
+			  | ((col_id >> 2) & 1u) << A2
+			  | ((col_id >> 3) & 1u) << A3;
+}
 
 void set_row_on(unsigned int row_id)
 {
 	// Set all rows OFF
-	PTA->PDOR &= ~( GPIO_PDOR_PDO( ROW_MASK ) );
-
-	PTA->PDOR |= GPIO_PDOR_PDO( 1 << ROW_ARR[row_id] );
-	//PTA->PDOR &= GPIO_PDOR_PDO( 1 << ROW_ARR[row_id] );
-}
-
-void display_field(void)
-{
-//	for (int r = 0; r < 8; ++r){
-//
-//	}
-
-	for (int i = 0; i < 128; ++i) {
-		if (field[i] == 0) {
-			continue;
-		}
-		//delay(tdelay1, tdelay2);
-		int row = i / 16;
-		int col = i % 16;
-
-		set_row_on(row);
-		set_col_on(col);
-	}
+	PTA->PCOR = ROW_MASK;
+	// Set the current row ON
+	PTA->PSOR = 1 << ROW_ARR[row_id];
 }
 
 
 void display_snake(void)
 {
-	for (int i = 0; i < SNAKE_LEN; ++i) {
-		//delay(tdelay1, tdelay2);
+	for (int i = SNAKE_LEN-1; i >=0; --i) {
+		delay(SNAKE_DISPLAY_LOOP_DELAY, 1);
 
 		set_row_on(snake_body[i].x);
 		set_col_on(snake_body[i].y);
@@ -336,8 +372,6 @@ void display_snake(void)
 
 void update_snake_pos(void)
 {
-
-
 	pos_t new_head_pos = { snake_body[0].x, snake_body[0].y };
 	switch (snake_dir) {
 	case UP: // Row 0 is the upper one
@@ -369,6 +403,7 @@ void update_snake_pos(void)
 }
 
 
+
 int main(void)
 {
 	/* Turn on all port clocks */
@@ -377,48 +412,14 @@ int main(void)
 	init_led();
 	init_buttons();
 	init_display();
+	init_pit();
 
 	play_led_sequence();
 
 	snake_dir = STOP;
-	unsigned int snake_delay = tdelay1 * tdelay2;
+	//unsigned int snake_delay = tdelay1 * tdelay2;
 	for (;;) {
-		counter++;
-
-		//delay(tdelay1, tdelay2);
-		// Set all cols OFF
-		//PTA->PDOR &= ~( GPIO_PDOR_PDO( COL_MASK ) );
-
-//		if (counter % 2 == 0) {
-//			set_col_on(0);
-//		} else {
-//			set_col_on(5);
-//			//PTA->PDOR |= GPIO_PDOR_PDO( 1 << A1 ); // set to 0010
-//			//PTA->PDOR |= GPIO_PDOR_PDO( 1 << A0 ); // set to 0001
-//		}
-		//display_field();
-
-
-
-		if (counter > snake_delay) {
-			counter = 0;
-
-			buttons_handler();
-
-			if (snake_dir >= UP && snake_dir <= RIGHT) {
-				update_snake_pos();
-			}
-			display_snake();
-		}
-
-
-
-
-		//play_led_sequence();
-		//play_display_row_sequence();
-		//play_display_col_sequence();
-
-		//handle_led_buttons();
+		display_snake();
     }
     /* Never leave main */
     return 0;
